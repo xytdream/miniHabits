60588a7c0d926f239043dcccc521869e43f78890
xiangyuntian <xiangyuntian@yunqian.tech>
2020-12-15 10:17:42 +0800

    init

---
+++ miniprogram/pages/sentence/sentence.js
@@ -0,0 +1,66 @@
+// pages/sentence/sentence.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    sentence: "见山是深情伟岸，见海是热情澎湃，见花见草信他们皆有故事，唯独见了你，山川沉默，海面静谧，花鸟鱼虫被光与尘凝固，世界万籁俱寂，只剩下你啊"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
